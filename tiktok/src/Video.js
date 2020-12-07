1afab6c56ae6e98a89d909cfd90f341d738927a2
mark-torres-10 <markptorres1@gmail.com>
2020-12-07 11:40:35 -0500

    add simple snapping functionality to videos

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,12 +1,30 @@
-import React from 'react'; 
+import React, { useRef , useState } from 'react'; 
 import "./Video.css"; 
 
 const Video = ({source}) => {
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef(null); 
+
+    const handleVideoPress = () => {
+        // if video is playing, stop it. 
+        if (playing) {
+            videoRef.current.pause(); 
+            setPlaying(false); 
+        } 
+        // otherwise, play it
+        else {
+            videoRef.current.play(); 
+            setPlaying(true); 
+        }
+    }
+
     return (
         <div className="video">
             <video 
+                onClick = {handleVideoPress}
                 className = "video_player"
                 loop = ""
+                ref = {videoRef}
                 src={source}></video>
             
             {/* VideoFooter */}
