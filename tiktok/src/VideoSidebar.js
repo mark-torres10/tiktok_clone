aba4ec22682964345048a8acfb107d4ff717781f
mark-torres-10 <markptorres1@gmail.com>
2020-12-07 13:12:36 -0500

    add mods to sidebar

--- tiktok/src/VideoSidebar.js
+++ tiktok/src/VideoSidebar.js
@@ -1,20 +1,37 @@
-import React from 'react'; 
+import React, { useState }from 'react'; 
 import './VideoSidebar.css'; 
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder"; 
 import MessageIcon from "@material-ui/icons/Message"; 
 import ShareIcon from "@material-ui/icons/Share";  
 
 const VideoSidebar = () => {
+
+    // add liking functionality
+    const [liked, setLiked] = useState(false); 
+
     return (
         <div className = "videoSidebar">
             <div className="videoSidebar_button">
-                <FavoriteIcon fontSize="large" />
+                {liked ? (
+                    <FavoriteIcon 
+                        fontSize="large" 
+                        onClick = {(e) => setLiked(false)}/>
+                ) : (
+                    <FavoriteBorderIcon 
+                        fontSize="large"
+                        onClick={(e) => setLiked(true)}
+                    />
+                )}
+                <p>{liked ? 101 : 100}</p>
             </div>
             <div className="videoSidebar_button">
                 <MessageIcon fontSize="large" />
+                <p>250</p>
             </div>
             <div className="videoSidebar_button">
                 <ShareIcon fontSize="large" />
+                <p>80</p>
             </div>
         </div>
     )
